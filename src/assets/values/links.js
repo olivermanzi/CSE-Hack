b5f684473439f7530a294779a970f545ca97ea85
Trooper98 <oliver.g.ryan@gmail.com>
2019-03-26 18:39:01 +0100

    (WIP) added signup button and removed a lot of unnecessary things but I have issues with the footers. For some reason, if i add the footer as a subcomponent of the base component (app.js), then it doesnt show. but if i add the html then it shows, but none of the links works. Regardsless, the register button that francisco wanted is up and all we need is a link.

---
+++ src/assets/values/links.js
@@ -0,0 +1,7 @@
+var facebook_link = "https://www.facebook.com/ctrlaltteam/";
+var mail_link = "mailto:cat@gota.gu.se?";
+
+module.exports = {
+    facebook_link,
+    mail_link
+}
---
+++ src/components/sub_components/register_button.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="col-sm-4 offset-sm-4 signup">
+    <button type="button" class="btn btn-success btn-lg">Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sign-up"
+};
+</script>
+
+<style scoped>
+@import "./../../assets/style/temp_style/temp_button.css";
+</style>
