a1bbbfa56bc014cad936caf4ed181b01662385ef
Trooper98 <oliver.g.ryan@gmail.com>
2019-03-26 18:43:32 +0100

    (WIP) hooked up the register button to the link asset (src/assets/values/links.js). Once the link is recived, all you have to do is change the string associated with the register_link variable

--- src/components/sub_components/register_button.vue
+++ src/components/sub_components/register_button.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="col-sm-4 offset-sm-4 signup">
-    <button type="button" class="btn btn-success btn-lg">Register</button>
+    <button type="button" class="btn btn-success btn-lg" v-bind:href="register_link">Register</button>
   </div>
 </template>
 
 <script>
+import links from "./../../assets/values/links.js";
 export default {
-  name: "sign-up"
+  name: "sign-up",
+  data() {
+    return {
+      register_link: link.register_link
+    };
+  }
 };
 </script>
 
