f2e7858ffff7f0db54098b7cf7b7a35170be1985
Naief Jobsen <gusjobna@student.gu.se>
2019-04-04 14:12:02 +0200

    Formating fix

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,7 @@ import partners from '@/components/partners.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'Banner',
       component: Banner
@@ -24,7 +23,7 @@ export default new Router({
       path: '/terms',
       name: 'Terms',
       component: terms
-    }, 
+    },
     {
       path: '/cases',
       name: 'Cases',
@@ -37,3 +36,4 @@ export default new Router({
     }
   ]
 })
+
