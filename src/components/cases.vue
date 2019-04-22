16f6fdc15ad8228f69c51061f84cab54695e94ab
Hanien <hanienkobus@gmail.com>
2019-04-22 14:49:30 +0200

    case cards on main cases page and modals open for a case with extended description

--- src/components/cases.vue
+++ src/components/cases.vue
@@ -9,61 +9,28 @@
                 </div>
             </div>
             <!-- TODO: Template below for the cases once they're announced -->
-            <!--<div class="row">
+            <div class="row">
                 <div class="col-sm-10 offset-sm-1">
                     <div class="card-columns">
                         <div class="card text-white bg-dark mb-3" style="max-width: 100%;">
-                            <div>
+                            <!--<div>
                                 <img class="card-img-top" src="img/stock/event.jpg" alt="event">
-                            </div>
-                            <div class="card-body">
-                                <h5 class="card-title">Case title</h5>
-                                <p class="card-text">
-                                    <hr>
-                                    <a class="btn btn-primary" data-toggle="modal" href="#case-2" role="button" aria-expanded="false" aria-controls="case-2">
-                                        Find out more...
-                                    </a>
-                                </p>
-                                <div class="modal fade" id="case-2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                                    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                                        <div class="modal-content" id="case-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalCenterTitle">Inclusive Multicultural Society</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <p>
-                                                    <strong>Description</strong>
-                                                    <br>
-                                                    <hr>
-                                                    <strong>Related Questions</strong>
-                                                    <br>
-                                                </p>
-                                                <ul>
-                                                    <li>How would you make learning Swedish a passive process rather than an
-                                                        active one?</li>
-                                                    <li>Is it possible to use technology to facilitate communication between
-                                                        workers from different backgrounds?
-                                                    </li>
-                                                    <li>Is it possible to accomplish the above in real time?</li>
-                                                    <li>Can we use technology to educate new arrivals about Swedish cultural
-                                                        norms?
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            </div>-->
+                            <case_example></case_example>
                         </div>
                     </div>
                 </div>
-            </div>-->
+            </div>
         </div>
     </div>
 </template>
+
+<script>
+import case_example from "./sub_components/cases/case_example";
+export default {
+  name: "Cases",
+  components: {
+    case_example
+  }
+};
+</script>
